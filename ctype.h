94f489209edfe490547ac968d205b6622dcd7343
JIANG Di <jiang-di@outlook.com>
2015-10-27 11:42:56 +0800

    :art:

--- ctype.h
+++ ctype.h
@@ -1,14 +1,14 @@
 #ifndef DEIDARA_CTYPE
 #define DEIDARA_CTYPE
 
-#define _U 0x01  /* upper */
-#define _L 0x02  /* lower */
-#define _D 0x04  /* digit */
-#define _C 0x08  /* cntrl */
-#define _P 0x10  /* punct */
-#define _S 0x20  /* white space (space/lf/tab) */
-#define _X 0x40  /* hex digit */
-#define _SP 0x80 /* hard space (0x20) */
+#define _U 0x01  // upper
+#define _L 0x02  // lower
+#define _D 0x04  //digit
+#define _C 0x08  // cntrl
+#define _P 0x10  // punct
+#define _S 0x20  // white space (space/lf/tab)
+#define _X 0x40  //hex digit
+#define _SP 0x80 // hard space (0x20)
 
 extern const unsigned char _ctype[];
 
@@ -22,7 +22,6 @@ extern const unsigned char _ctype[];
 #define islower(c) ((__ismask(c) & (_L)) != 0)
 #define isprint(c) ((__ismask(c) & (_P | _U | _L | _D | _SP)) != 0)
 #define ispunct(c) ((__ismask(c) & (_P)) != 0)
-/* Note: isspace() must return false for %NUL-terminator */
 #define isspace(c) ((__ismask(c) & (_S)) != 0)
 #define isupper(c) ((__ismask(c) & (_U)) != 0)
 #define isxdigit(c) ((__ismask(c) & (_D | _X)) != 0)
